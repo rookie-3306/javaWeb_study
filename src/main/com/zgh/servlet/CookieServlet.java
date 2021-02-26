609f6e05d8592bd802eb8c35c70b1336bd1748e0
rookie-3306 <2821537895@qq.com>
2021-02-26 18:40:03 +0800

    cookie的生命周期

--- src/main/com/zgh/servlet/CookieServlet.java
+++ src/main/com/zgh/servlet/CookieServlet.java
@@ -1,5 +1,6 @@
 package main.com.zgh.servlet;
 
+import com.sun.crypto.provider.PBEWithMD5AndDESCipher;
 import main.com.zgh.util.WebUtil;
 
 import javax.servlet.ServletException;
@@ -10,7 +11,7 @@ import java.io.IOException;
 
 public class CookieServlet extends BaseServlet {
     public void createCookie(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-        Cookie cookie = new Cookie("key2","value2");
+        Cookie cookie = new Cookie("key1","value1");
         resp.addCookie(cookie);
         resp.getWriter().write("创建Cookie成功!");
     }
@@ -22,6 +23,7 @@ public class CookieServlet extends BaseServlet {
             resp.getWriter().write("<br/>");
         }
     }
+
     public void getWantCookie(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
         Cookie[] cookies = req.getCookies();
         Cookie cookie = WebUtil.getWantCookie("key1",cookies);
@@ -30,6 +32,32 @@ public class CookieServlet extends BaseServlet {
         }
         else{
             resp.getWriter().write("想要的cookie值为 => " + cookie.getValue());
+        }
+    }
+
+    public void updateCookie(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        Cookie cookie = WebUtil.getWantCookie("key1",req.getCookies());
+        if(cookie != null){
+            resp.getWriter().write("获取了对应的Cookie,原来的值是 => " + cookie.getValue());
+            resp.getWriter().write("<br/>");
+            cookie.setValue("newKey1");
+            resp.addCookie(cookie);
+            Cookie newCookie = WebUtil.getWantCookie("key1",req.getCookies());
+            resp.getWriter().write("获取了对应的Cookie,新的值是 => " + newCookie.getValue());
+        }
+        else {
+            resp.getWriter().write("不存在对应Cookie!");
+        }
+    }
+
+    public void deleteCookieNew(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        Cookie cookie = WebUtil.getWantCookie("key1",req.getCookies());
+        if (cookie != null){
+            cookie.setMaxAge(0);
+            resp.getWriter().write("删除Cookie成功!");
+        }
+        else {
+            resp.getWriter().write("不存在对应Cookie!");
         }
     }
 }
--- src/main/webapp/cookie.jsp
+++ src/main/webapp/cookie.jsp
@@ -14,5 +14,7 @@
     <a href="cookieServlet?action=createCookie">创建Cookie</a><br/>
     <a href="cookieServlet?action=getAllCookie">获取所有Cookie</a><br/>
     <a href="cookieServlet?action=getWantCookie">获取指定名字的Cookie</a><br/>
+    <a href="cookieServlet?action=updateCookie">更新对应Cookie</a><br/>
+    <a href="cookieServlet?action=deleteCookieNew">删除对应Cookie</a><br/>
 </body>
 </html>
